2bc6d5da6bdf8895563ac907ee097964453f9689
Ulugbek999 <ulugbekabdurakhmonov.tennis@gmail.com>
2025-07-18 18:54:12 -0400

    Database Entities for Users, UserTypes created

---
+++ src/main/java/com/easywork/jobportal/entity/UsersType.java
@@ -0,0 +1,74 @@
+package com.easywork.jobportal.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name="users_type")
+public class UsersType {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private int userTypeId;
+    private String userTypeName;
+
+    @OneToMany(targetEntity = Users.class, mappedBy = "userTypeId", cascade = CascadeType.ALL)
+    private List<Users> users;
+
+    public UsersType(int userTypeId, String userTypeName, List<Users> users) {
+        this.userTypeId = userTypeId;
+        this.userTypeName = userTypeName;
+        this.users = users;
+    }
+
+    public int getUserTypeId() {
+        return userTypeId;
+    }
+
+    public void setUserTypeId(int userTypeId) {
+        this.userTypeId = userTypeId;
+    }
+
+    public String getUserTypeName() {
+        return userTypeName;
+    }
+
+    public void setUserTypeName(String userTypeName) {
+        this.userTypeName = userTypeName;
+    }
+
+    public List<Users> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<Users> users) {
+        this.users = users;
+    }
+
+    @Override
+    public String toString() {
+        return "UsersType{" +
+            "userTypeId = " + userTypeId +
+            ", userTypeName = '" + userTypeName + '\'' +
+        "}";
+    }
+
+
+ 
+}
