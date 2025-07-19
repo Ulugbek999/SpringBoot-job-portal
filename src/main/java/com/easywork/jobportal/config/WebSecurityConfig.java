c22109229c04a12823c0b8c7bc7678ead90ae69d
Ulugbek999 <ulugbekabdurakhmonov.tennis@gmail.com>
2025-07-19 16:28:49 -0400

    Custom User Authentication and Authorization added.

--- src/main/java/com/easywork/jobportal/config/WebSecurityConfig.java
+++ src/main/java/com/easywork/jobportal/config/WebSecurityConfig.java
@@ -1,12 +1,27 @@
 package com.easywork.jobportal.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.easywork.jobportal.services.CustomUserDetailsService;
 
 @Configuration
 public class WebSecurityConfig {
+
+    private final CustomUserDetailsService customUserDetailsService;
+
+
+    @Autowired
+    public WebSecurityConfig(CustomUserDetailsService customUserDetailsService){
+        this.customUserDetailsService = customUserDetailsService;
+    }
     
 
     private final String[] publicUrl = {"/",
@@ -27,6 +42,10 @@ public class WebSecurityConfig {
     @Bean
     protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 
+
+        http.authenticationProvider(authenticationProvider());
+
+
         http.authorizeHttpRequests(auth -> {
             auth.requestMatchers(publicUrl).permitAll();
             auth.anyRequest().authenticated();
@@ -34,5 +53,24 @@ public class WebSecurityConfig {
 
 
         return http.build();
+    }
+
+    @Bean
+    public  AuthenticationProvider authenticationProvider() {
+        //custom authentication provider (telling spring how to find the users and how to authenticate passwords)
+
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        authenticationProvider.setUserDetailsService(customUserDetailsService);
+
+        return authenticationProvider;
+    
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        //telling spring how to authenticate passwords;
+
+        return new BCryptPasswordEncoder();
     }
 }
---
+++ src/main/java/com/easywork/jobportal/services/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.easywork.jobportal.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.easywork.jobportal.entity.Users;
+import com.easywork.jobportal.repository.UsersRepository;
+import com.easywork.jobportal.util.CustomUserDetails;
+
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService{
+
+    private final UsersRepository usersRepository;
+    
+    @Autowired
+    public CustomUserDetailsService(UsersRepository usersRepository){
+        this.usersRepository = usersRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Users user = usersRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Could not find the user. "));
+        return new CustomUserDetails(user);
+    }
+    
+}
